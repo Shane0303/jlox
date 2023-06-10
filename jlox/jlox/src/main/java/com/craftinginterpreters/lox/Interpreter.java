bfe83419a153e58217206ac7251c3bcd9ff3ccb4
Shane0303 <21h52.shane@sjec.ac.in>
2023-06-10 16:50:39 +0530

    changed

--- jlox/jlox/src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/jlox/src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -4,31 +4,53 @@
  */
 package com.craftinginterpreters.lox;
 
-/**
- *
- * @author sharon
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
+import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>,
                              Stmt.Visitor<Void> {
-    private final Environment environment = new Environment();
+    private Environment environment = new Environment();
+    void interpret(List<Stmt> statements) {
+    try {
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
 
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
     }
+    public Object visitLogicalExpr(Expr.Logical expr) {
+    Object left = evaluate(expr.left);
+
+    if (expr.operator == TokenType.OR) {
+        if (isTruthy(left)) return left;
+    } else {
+        if (!isTruthy(left)) return left;
+    }
+
+    return evaluate(expr.right);
+}
+
     @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
 
         switch (expr.operator.type) {
-            case BANG -> {
-                return !isTruthy(right);
-            }
-            case MINUS -> {
+            case BANG:
+            return !isTruthy(right);
+            case MINUS:
                 checkNumberOperand(expr.operator, right);
-                return -(double)right;
-            }
+            return -(double)right;
         }
 
         // Unreachable.
@@ -78,6 +100,28 @@ class Interpreter implements Expr.Visitor<Object>,
     private Object evaluate(Expr expr) {
         return expr.accept(this);
     }
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
+    }
+    void executeBlock(List<Stmt> statements,
+                    Environment environment) {
+    Environment previous = this.environment;
+    try {
+      this.environment = environment;
+
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } 
+      finally {
+            this.environment = previous;
+      }
+    }
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
 
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
@@ -85,11 +129,22 @@ class Interpreter implements Expr.Visitor<Object>,
         return null;
     }
     @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+      if (isTruthy(evaluate(stmt.condition))) {
+        execute(stmt.thenBranch);
+      } else if (stmt.elseBranch != null) {
+        execute(stmt.elseBranch);
+      }
+      return null;
+    }
+
+    @Override
     public Void visitPrintStmt(Stmt.Print stmt) {
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
         return null;
     }
+
     @Override
     public Void visitVarStmt(Stmt.Var stmt) {
         Object value = null;
@@ -101,66 +156,62 @@ class Interpreter implements Expr.Visitor<Object>,
         return null;
     }
     @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+      while (isTruthy(evaluate(stmt.condition))) {
+        execute(stmt.body);
+      }
+      return null;
+    }   
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+        Object value = evaluate(expr.value);
+        environment.assign(expr.name, value);
+        return value;
+    }
+    @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object left = evaluate(expr.left);
         Object right = evaluate(expr.right); 
 
         switch (expr.operator.type) {
-            case GREATER -> {
+            case GREATER:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left > (double)right;
-            }
-            case GREATER_EQUAL -> {
+            return (double)left > (double)right;
+            case GREATER_EQUAL:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left >= (double)right;
-            }
-            case LESS -> {
+            return (double)left >= (double)right;
+            case LESS:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left < (double)right;
-            }
-            case LESS_EQUAL -> {
+            return (double)left < (double)right;
+            case LESS_EQUAL:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left <= (double)right;
-            }
-            case MINUS -> {
+            return (double)left <= (double)right;
+            case MINUS:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left - (double)right;
+            return (double)left - (double)right;
+            case PLUS:
+            if (left instanceof Double && right instanceof Double) {
+                return (double)left + (double)right;
+            } 
+
+            if (left instanceof String && right instanceof String) {
+                return (String)left + (String)right;
             }
-            case PLUS -> {
-                if (left instanceof Double && right instanceof Double) {
-                    return (double)left + (double)right;
-                }
-                
-                if (left instanceof String && right instanceof String) {
-                    return (String)left + (String)right;
-                }
-                
-                throw new RuntimeError(expr.operator,
-                        "Operands must be two numbers or two strings.");
-            }
-            case SLASH -> {
+
+            throw new RuntimeError(expr.operator,
+            "Operands must be two numbers or two strings.");
+            case SLASH:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left / (double)right;
-            }
-            case STAR -> {
+            return (double)left / (double)right;
+            case STAR:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left * (double)right;
-            }
-            case BANG_EQUAL -> {
-                return !isEqual(left, right);
-            }
-            case EQUAL_EQUAL -> {
-                return isEqual(left, right);
-            }
+            return (double)left * (double)right;
+            case BANG_EQUAL: return !isEqual(left, right);
+            case EQUAL_EQUAL: return isEqual(left, right);
         }
 
         // Unreachable.
         return null;
-    }
-
-    @Override
-    public Void visitBlockStmt(Stmt.Block stmt) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
@@ -179,17 +230,7 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
-    public Void visitIfStmt(Stmt.If stmt) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
     public Void visitReturnStmt(Stmt.Return stmt) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public Void visitWhileStmt(Stmt.While stmt) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
