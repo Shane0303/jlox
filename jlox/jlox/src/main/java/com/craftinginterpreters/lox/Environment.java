5593d931b30bd63afb462c0a15890600a6274e2f
Shane0303 <21h52.shane@sjec.ac.in>
2023-06-11 16:27:52 +0530

    made changes

--- jlox/jlox/src/main/java/com/craftinginterpreters/lox/Environment.java
+++ jlox/jlox/src/main/java/com/craftinginterpreters/lox/Environment.java
@@ -4,7 +4,6 @@
  */
 package com.craftinginterpreters.lox;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -40,6 +39,20 @@ class Environment {
   }
   void define(String name, Object value) {
     values.put(name, value);
+  }
+  Environment ancestor(int distance) {
+    Environment environment = this;
+    for (int i = 0; i < distance; i++) {
+      environment = environment.enclosing; 
+    }
+
+    return environment;
+  }
+  Object getAt(int distance, String name) {
+    return ancestor(distance).values.get(name);
+  }
+  void assignAt(int distance, Token name, Object value) {
+    ancestor(distance).values.put(name.lexeme, value);
   }
 
     void assign(Object name, Object value) {
